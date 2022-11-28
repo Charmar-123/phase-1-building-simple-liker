521432cef78cd386e590d11b190ffc058ff41e08
Charbel Maroun <charbel-maroun@hotmail.com>
2022-11-28 00:26:55 -0500

    commit tree 05878523e92e60e8efbdf8851d1c1b9500f1f245

--- main.js
+++ main.js
@@ -11,9 +11,9 @@ const FULL_HEART = '♥'
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
       let isRandomFailure = Math.random() < .2
       if (isRandomFailure) {
         reject("Random server error. Try again.");
@@ -23,3 +23,36 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
+const errMessage = document.querySelector("#modal")
+errMessage.className = "hidden"
+
+const heart = document.querySelector(".like-glyph")
+
+heart.addEventListener("click", (event) => {
+  console.log(event)
+  const serverRes = mimicServerCall()
+  serverRes.then((data) => {
+    console.log(data)
+    if (heart.className === "activated-heart") {
+      return heart.className = ""
+    }
+    else {
+      return heart.className = "activated-heart"
+    }
+  }
+  )
+
+  serverRes.catch((data) => {
+    console.log(data)
+    return errMessage.className = " "
+  })
+  console.log(mimicServerCall());
+  setTimeout(() => errMessage.className = "hidden", 3000) 
+})
+
+// mimicServerCall()
+// .then()
+// .catch()
+  // serverRes.then((data) => {if (data.PromiseState = "fulfilled") {return heart.className = "activated-heart"}})
+  // serverRes.catch((data) => {if (data.PromiseState = "rejected") {return errMessage.className = ""}})
